703497bef072be4a1db0a9a102c66c96bd71ff27
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-02-02 09:54:50 +0700

    created navbar

---
+++ lib/screens/transactions/screen_transaction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Screentransaction extends StatefulWidget {
+  const Screentransaction({super.key});
+
+  @override
+  State<Screentransaction> createState() => _ScreentransactionState();
+}
+
+class _ScreentransactionState extends State<Screentransaction> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/transactions/widget/histroy.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HistroyPage extends StatefulWidget {
+  const HistroyPage({super.key});
+
+  @override
+  State<HistroyPage> createState() => _HistroyPageState();
+}
+
+class _HistroyPageState extends State<HistroyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
