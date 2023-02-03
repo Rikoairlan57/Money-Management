543eabf9d1fc12dbcf2f265fdf167548a03c006e
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-02-04 06:37:01 +0700

    🔒️ 🗃️ injection db

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:money_management/screens/basescreen/splash_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:money_management/db/transaction/transaction_db.dart';
+import 'package:money_management/models/transaction_model/transaction_model.dart';
+import 'package:money_management/models/category_model/category_model.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(CategoryTypeAdapter().typeId)) {
+    Hive.registerAdapter(CategoryTypeAdapter());
+  }
+  if (!Hive.isAdapterRegistered(CategoryModelAdapter().typeId)) {
+    Hive.registerAdapter(CategoryModelAdapter());
+  }
+  if (!Hive.isAdapterRegistered(TransactionModelAdapter().typeId)) {
+    Hive.registerAdapter(TransactionModelAdapter());
+  }
+
   runApp(const MyApp());
+  TransactionDb.instance.refreshUI();
 }
 
 class MyApp extends StatelessWidget {
