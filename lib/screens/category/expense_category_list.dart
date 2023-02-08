005d07a15d5a277310f582677f72143b1e2f06c5
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-02-08 08:58:40 +0700

    ✨ 💄 🎨 created ui category list and graph

---
+++ lib/screens/category/expense_category_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpenseCategoryList extends StatelessWidget {
+  const ExpenseCategoryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
